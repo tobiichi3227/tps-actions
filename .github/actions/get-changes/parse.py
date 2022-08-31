f2963c9c7b0b4388bea06142b8369e92187938a4
Xi-Plus <huangxuanyuxiplus@gmail.com>
2022-08-31 23:30:46 +0800

    Add run-problems

--- .github/actions/get-changes/parse.py
+++ .github/actions/get-changes/parse.py
@@ -5,36 +5,107 @@ os.chdir(os.environ.get('GITHUB_WORKSPACE'))
 
 problems = json.loads(os.environ.get('PROBLEMS'))
 changes = json.loads(os.environ.get('CHANGES'))
-keys = ['input', 'solutions', 'pdf', 'any']
+flagpath = os.environ.get('FLAGPATH')
+keys = ['input', 'solutions', 'pdf', 'verify']
 prefixes = {}
 for pro in problems:
     prefixes['p{}/'.format(pro)] = pro
 
-result = {}
-result['template'] = changes['template']
-result['cover'] = changes['cover']
+flags = {
+    'tests': {},
+    'input': {},
+    'output': {},
+    'pdf': {},
+    'verify': {},
+}
 for key in keys:
-    result[key] = {}
     for pro in problems:
-        result[key][pro] = 'false'
-        if key == 'pdf':
-            result[key][pro] = changes['template']
-    for file in json.loads(changes['{}_files'.format(key)]):
-        for prefix, pro in prefixes.items():
-            if file.startswith(prefix):
-                result[key][pro] = 'true'
-                break
-result['output'] = {}
+        flags[key][pro] = False
+
+# tests, input, output
+input_files = json.loads(changes['input_files'])
 solutions_files = json.loads(changes['solutions_files'])
 for pro in problems:
-    result['output'][pro] = 'false'
+    if os.path.exists('p{}/gen/DISABLE_AUTO_BUILD'.format(pro)):
+        print('Skip tests for p{} due to disable flag'.format(pro))
+        continue
+
+    # push
+    prefix = 'p{}/'.format(pro)
+
+    for file in input_files:
+        if file.startswith(prefix):
+            flags['tests'][pro] = True
+            flags['input'][pro] = True
+            print('Set tests,input/{} to true due to {}'.format(pro, file))
+            break
+
+    for file in solutions_files:
+        if file.startswith(prefix):
+            flags['tests'][pro] = True
+            print('Set tests/{} to true due to {}'.format(pro, file))
+            break
+
     with open('p{}/solutions.json'.format(pro), 'r', encoding='utf8') as f:
         solutions = json.load(f)
     for file, val in solutions.items():
         if val.get('verdict') == 'model_solution':
             if 'p{}/solution/{}'.format(pro, file) in solutions_files:
-                result['output'][pro] = 'true'
-                result['solutions'][pro] = 'true'
+                flags['tests'][pro] = True
+                flags['output'][pro] = not flags['input'][pro]
+                print('Set tests,output/{0} to true due to p{0}/{1}'.format(pro, file))
             break
 
+    # dispatch
+    if os.path.exists(os.path.join(flagpath, 'input-{}'.format(pro))):
+        flags['input'][pro] = True
+        print('Set input/{} to true due to config'.format(pro))
+    if os.path.exists(os.path.join(flagpath, 'output-{}'.format(pro))):
+        flags['output'][pro] = True
+        print('Set output/{} to true due to config'.format(pro))
+    if os.path.exists(os.path.join(flagpath, 'solutions-{}'.format(pro))):
+        flags['solutions'][pro] = True
+        print('Set solutions/{} to true due to config'.format(pro))
+
+# pdf
+for pro in problems:
+    if os.path.exists('p{}/statement/DISABLE_AUTO_BUILD'.format(pro)):
+        print('Skip pdf for p{} due to disable flag'.format(pro))
+        continue
+
+    prefix = 'p{}/'.format(pro)
+
+    for file in json.loads(changes['pdf_files']):
+        if file.startswith(prefix):
+            flags['pdf'][pro] = True
+            print('Set pdf/{} to true due to {}'.format(pro, file))
+            break
+
+    if changes['template'] == 'true':
+        flags['pdf'][pro] = True
+        print('Set pdf/{} to true due to template'.format(pro))
+
+    if pro == 'A' and changes['cover'] == 'true':
+        flags['pdf']['A'] = True
+        print('Set pdf/A to true due to cover')
+
+# verify
+verify_files = json.loads(changes['verify_files'])
+for pro in problems:
+    prefix = 'p{}/'.format(pro)
+    for file in verify_files:
+        if file.startswith(prefix):
+            flags['verify'][pro] = True
+            print('Set verify/{} to true due to {}'.format(pro, file))
+            break
+
+result = {}
+for key in flags:
+    result[key] = ''
+    for pro in problems:
+        if flags[key][pro]:
+            result[key] += pro
+
+print('flags:', flags)
+print('result:', result)
 print('::set-output name=changes::{}'.format(json.dumps(result)))
---
+++ .github/actions/run-problems/filter.py
@@ -0,0 +1,15 @@
+import json
+import os
+
+requests = os.environ.get('REQUESTS')
+problems = json.loads(os.environ.get('PROBLEMS'))
+
+print('requests:', requests)
+
+result = []
+for pro in problems:
+    if pro in requests:
+        result.append(pro)
+
+print('result:', result)
+print('::set-output name=problems::{}'.format(json.dumps(result)))
