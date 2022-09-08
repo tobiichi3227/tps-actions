88bb74cb150e83f8bf910cce6d76d2f8dca664cb
Xi-Plus <huangxuanyuxiplus@gmail.com>
2022-09-08 20:59:55 +0800

    Add check-progress and merge list-subtasks to generate-report

---
+++ .github/actions/check-progress/check.py
@@ -0,0 +1,139 @@
+import glob
+import json
+import os
+
+os.chdir(os.environ.get('GITHUB_WORKSPACE'))
+
+with open('.problems.json', 'r', encoding='utf8') as f:
+    problems = json.load(f)
+
+output = ''
+output += '| |'
+for pro in problems:
+    output += ' {} |'.format(pro)
+output += '\n'
+
+output += '|'
+for _ in range(len(problems) + 1):
+    output += ' --- |'
+output += '\n'
+
+# problem.json
+problemjson = {}
+for pro in problems:
+    with open('p{}/problem.json'.format(pro), 'r', encoding='utf8') as f:
+        problemjson[pro] = json.load(f)
+
+# subtasks.json
+subtasksjson = {}
+for pro in problems:
+    with open('p{}/subtasks.json'.format(pro), 'r', encoding='utf8') as f:
+        subtasksjson[pro] = json.load(f)
+
+# problem info
+keys = [
+    'contest_name',
+    'problem_label',
+    'name',
+    'title',
+]
+for key in keys:
+    output += '| {} | '.format(key)
+    for pro in problems:
+        text = ''
+        if isinstance(problemjson[pro][key], str) and 'TODO' in problemjson[pro][key]:
+            icon = ':x:'
+        else:
+            icon = ':white_check_mark:'
+            if key not in ['contest_name', 'problem_label']:
+                text = '<br>{}'.format(problemjson[pro][key])
+        output += ' [{}](p{}/problem.json){} |'.format(icon, pro, text)
+    output += '\n'
+
+keys = [
+    'memory_limit',
+    'time_limit',
+    'has_checker',
+]
+for key in keys:
+    output += '| {} | '.format(key)
+    for pro in problems:
+        output += ' {} |'.format(problemjson[pro][key])
+    output += '\n'
+
+# gen/solution/validator
+folders = [
+    'gen',
+    'solution',
+    'validator',
+]
+for folder in folders:
+    output += '| {} |'.format(folder)
+    for pro in problems:
+        todos = []
+        for file in glob.glob('p{}/{}/**'.format(pro, folder), recursive=True):
+            if os.path.isdir(file):
+                continue
+            with open(file, 'r', encoding='utf8') as f:
+                try:
+                    content = f.read()
+                except Exception as e:
+                    print('Ignore {}'.format(file))
+                    continue
+                if 'TODO' in content:
+                    todos.append(file)
+        if len(todos) == 0:
+            output += ' [:white_check_mark:](p{}/{}) |'.format(pro, folder)
+        else:
+            output += ' [:x:](p{}/{})'.format(pro, folder)
+            for file in todos:
+                output += '<br>[{}]({})'.format(os.path.basename(file), file)
+            output += ' |'
+    output += '\n'
+
+# global_validators
+output += '| subtasks.json<br>global_validators | '
+for pro in problems:
+    if len(subtasksjson[pro]['global_validators']) == 0:
+        icon = ':warning:'
+        text = ' Not set'
+    else:
+        icon = ':white_check_mark:'
+        text = ''
+    output += ' [{}](p{}/subtasks.json){} |'.format(icon, pro, text)
+output += '\n'
+
+# statement
+output += '| statement |'
+for pro in problems:
+    with open('p{}/statement/index.md'.format(pro), 'r', encoding='utf8') as f:
+        content = f.read()
+
+    if 'TODO' in content:
+        icon = ':x:'
+    else:
+        icon = ':white_check_mark:'
+    output += ' [{}](p{}/statement/index.md) |'.format(icon, pro)
+output += '\n'
+
+reportpath = os.environ.get('REPORTPATH')
+
+try:
+    with open(reportpath, 'r', encoding='utf8') as f:
+        text = f.read()
+except FileNotFoundError:
+    text = ''
+
+flag1 = '<!-- progress start -->'
+flag2 = '<!-- progress end -->'
+try:
+    idx1 = text.index(flag1)
+    idx2 = text.index(flag2)
+except ValueError:
+    text += '\n## Progress\n{}\n{}\n'.format(flag1, flag2)
+    idx1 = text.index(flag1)
+    idx2 = text.index(flag2)
+
+text = text[:idx1] + flag1 + '\n' + output + text[idx2:]
+with open(reportpath, 'w', encoding='utf8') as f:
+    f.write(text)
---
+++ .github/actions/list-subtasks/list.py
@@ -0,0 +1,60 @@
+import json
+import os
+
+os.chdir(os.environ.get('GITHUB_WORKSPACE'))
+
+with open('.problems.json', 'r', encoding='utf8') as f:
+    problems = json.load(f)
+
+all_subtasks = {}
+max_subtask = 0
+for pro in problems:
+    with open('p{}/subtasks.json'.format(pro), 'r', encoding='utf8') as f:
+        subtasks = json.load(f)['subtasks']
+
+    all_subtasks[pro] = []
+    for subtask in sorted(subtasks.values(), key=lambda v: v['index']):
+        if subtask['index'] == 0:
+            continue
+        all_subtasks[pro].append((subtask['score'], subtask['text']))
+
+    max_subtask = max(max_subtask, len(all_subtasks[pro]))
+
+output = ''
+output += '| |'
+for i in range(max_subtask):
+    output += ' {} |'.format(i + 1)
+output += '\n'
+
+output += '|'
+for i in range(max_subtask + 1):
+    output += ' --- |'
+output += '\n'
+
+for pro in problems:
+    output += '| {} |'.format(pro)
+    for subtask in all_subtasks[pro]:
+        output += ' {}<br>{} |'.format(subtask[0], subtask[1])
+    output += '\n'
+
+reportpath = os.environ.get('REPORTPATH')
+
+try:
+    with open(reportpath, 'r', encoding='utf8') as f:
+        text = f.read()
+except FileNotFoundError:
+    text = ''
+
+flag1 = '<!-- subtasks start -->'
+flag2 = '<!-- subtasks end -->'
+try:
+    idx1 = text.index(flag1)
+    idx2 = text.index(flag2)
+except ValueError:
+    text += '\n## Subtasks\n{}\n{}\n'.format(flag1, flag2)
+    idx1 = text.index(flag1)
+    idx2 = text.index(flag2)
+
+text = text[:idx1] + flag1 + '\n' + output + text[idx2:]
+with open(reportpath, 'w', encoding='utf8') as f:
+    f.write(text)
