7d1c4e2420342737399b2c13f99ca58e1439a033
Xi-Plus <huangxuanyuxiplus@gmail.com>
2022-09-17 15:47:33 +0800

    Add jekyll_config

--- .github/actions/new-problem/create.py
+++ .github/actions/new-problem/create.py
@@ -26,6 +26,9 @@ with open('Makefile', 'r', encoding='utf8') as f:
 with open('README.md', 'r', encoding='utf8') as f:
     readme = f.read()
 
+with open('_config.yml', 'r', encoding='utf8') as f:
+    jekyll_config = f.read()
+
 for i in range(prob_cnt):
     label = chr(ord('A') + i)
     path = 'p{}'.format(label)
@@ -42,12 +45,16 @@ for i in range(prob_cnt):
         subprocess.run(['git', 'add', path])
 
         problems.append(label)
-        makefile = makefile.replace('# NEWPROBELM', 'import-{0}:\n\tcmsImportTask ./{0}/ -u $(if $(s), , --no-statement)\n\n# NEWPROBELM'.format(path))
+
+        makefile = makefile.replace('# NEWPROBLEM', 'import-{0}:\n\tcmsImportTask ./{0}/ -u $(if $(s), , --no-statement)\n\n# NEWPROBLEM'.format(path))
+
         readme = re.sub(
             '\n*<!-- new problem -->',
             '\n| {0} | [statement]({1}/statement) [md]({1}/statement/index.md) [pdf]({1}/statement/index.pdf) | [gen]({1}/gen) | [validator]({1}/validator) | [solution]({1}/solution) [check]({1}/solutions-check.txt) | [tests]({1}/tests) | [problem]({1}/problem.json) [solutions]({1}/solutions.json) [subtasks]({1}/subtasks.json) |\n\n<!-- new problem -->'.format(label, path),
             readme
         )
+
+        jekyll_config = jekyll_config.replace('# NEWPROBLEM', '  - p{0}/scripts/\n  - p{0}/tests/\n# NEWPROBLEM'.format(label))
     else:
         print('{} is exists'.format(path))
 
@@ -59,3 +66,6 @@ with open('Makefile', 'w', encoding='utf8') as f:
 
 with open('README.md', 'w', encoding='utf8') as f:
     f.write(readme)
+
+with open('_config.yml', 'w', encoding='utf8') as f:
+    f.write(jekyll_config)
